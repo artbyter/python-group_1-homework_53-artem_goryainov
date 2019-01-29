168d6616bc838020667106da465ce4804f626d3e
Art <artbyter@gmail.com>
2019-01-29 22:17:46 +0600

    Added .env for realtime react updates

---
+++ src/nball.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+import 'nball.css'
+
+class NBall extends Component {
+    render() {
+        <div className="ball">
+            <h1>{this.props.number}</h1>
+        </div>
+    }
+}
