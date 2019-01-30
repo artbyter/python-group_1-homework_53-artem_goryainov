0d0143bca6a6284da18dab153522c0a66bdce771
Art <artbyter@gmail.com>
2019-01-30 12:09:50 +0600

    Creating final app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+
 import './App.css';
+import NBall  from './nball.js'
+
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {numbers: ["","","","",""]};
+
+    }
+
+    updateNumbers = () => {
+        let listNum = [...Array(32).keys()].map(item => (item + 5))
+        let list = [];
+        let num = 32
+        for (let i = 0; i < 5; i++) {
+
+            list.push(listNum.splice(Math.floor(Math.random() * num), 1));
+            num--;
+        }
+
+        this.setState({numbers: list.sort((a,b)=>a-b)})
+    }
+
+    render() {
+        return (
+            <div className="App">
+
+                <header><h1>Лоторея 5 из 36</h1></header>
+                <main>
+                    {this.state.numbers.map(item => (<NBall number={item}/>))}
+                </main>
+                <button className='numButton' onClick={this.updateNumbers}>Мне повезет!</button>
+            </div>
+        );
+    }
 }
+
 
 export default App;
